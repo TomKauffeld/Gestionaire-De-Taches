d14140a217ab81f6440dbb9bf79fa1b72900db7e
fldelcroix <fldelcroix@iutclinfa2311>
2017-11-15 11:13:46 +0100

    Ajout de la classe Tache

--- Gestionnaire de taches/src/gdt/assets/Tache.java
+++ Gestionnaire de taches/src/gdt/assets/Tache.java
@@ -1,6 +1,60 @@
 
 package gdt.assets;
 
+import java.time.LocalDate;
+
+
 public class Tache {
+    private String titre;
+    private String description;
+    private Boolean fini;
+    private long id;
+    private LocalDate begin;
+    private LocalDate end;
+    
+    protected Tache(String titre,String description,Boolean fini,long id,LocalDate begin,LocalDate end){
+        this.titre=titre;
+        this.description=description;
+        this.fini=fini;
+        this.id=id;
+        this.begin=begin;
+        this.end=end;
+    }
+    //getter
+    protected String getTitre(){ 
+        return titre;
+    }
+    protected String getDescription(){
+        return description;
+    }
+    protected Boolean getFini(){
+        return fini;
+    }
+    protected long getId(){
+        return id;
+    }
+    protected LocalDate getBegin(){
+        return begin;
+    }
+    protected LocalDate getEnd(){
+        return end;
+    }
+    
+    //setter
+    protected void setTitre(String titre){
+        this.titre=titre;
+    }
+    protected void setDescription(String description){
+        this.description=description;
+    }
+    protected void setFini(Boolean fini){
+        this.fini=fini;
+    }
+    protected void setBegin(LocalDate begin){
+        this.begin=begin;
+    }
+    protected void setEnd(LocalDate end){
+        this.end = end;
+    }
     
 }
