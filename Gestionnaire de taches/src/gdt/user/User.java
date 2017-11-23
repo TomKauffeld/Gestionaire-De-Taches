abc6d0956859440b92c9d8f0c3ccccba0ca9cfa2
TomKauffeld <33445304+TomKauffeld@users.noreply.github.com>
2017-11-23 08:22:45 +0100

    Changement de language naturele

--- Gestionnaire de taches/src/gdt/user/User.java
+++ Gestionnaire de taches/src/gdt/user/User.java
@@ -5,35 +5,43 @@ import java.io.Serializable;
 
 
 public class User implements Serializable{
+
+    public static final long GUEST_ID = -1;
+
     private long id;
-    private String motDePasse;
-    private String pseudo;
+    private String password;
+    private String username;
     
-    protected User(long id,String motDePasse,String pseudo){
-        this.id=id;
-        setMotDePasse( motDePasse);
-        this.pseudo=pseudo;
+    protected User( long id,String password,String username){
+        setId( id);
+        setPassword( password);
+        setUsername( username);
     }
 
-    public boolean passwordIsCorrect( String password){
-        return (motDePasse.equals( password));
+    public boolean isPasswordCorrect( String password){
+        return (getPassword().equals( password));
     }
-    
-    
-   //getter 
+
+   // Getters
     public long getId() {
         return id;
     }
-    public String getPseudo() {
-        return pseudo;
+    public String getUsername() {
+        return username;
+    }
+    private String getPassword( ){
+        return password;
     }
     
-    //setter
-    public void setMotDePasse(String motDePasse) {
-        this.motDePasse = motDePasse;
+    // Setters
+    private void setId( long id){
+        this.id = id;
     }
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
+    private void setPassword( String password) {
+        this.password = password;
+    }
+    private void setUsername(String username) {
+        this.username = username;
     }
     
     
