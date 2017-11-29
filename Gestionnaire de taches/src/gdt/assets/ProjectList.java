b3a202a3f19792443b1de1a177f3fc80e7392586
TomKauffeld <33445304+TomKauffeld@users.noreply.github.com>
2017-11-29 10:51:23 +0100

    changement du ProjectList

--- Gestionnaire de taches/src/gdt/assets/ProjectList.java
+++ Gestionnaire de taches/src/gdt/assets/ProjectList.java
@@ -2,24 +2,43 @@ package gdt.assets;
 
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 
 import java.io.Serializable;
 import java.util.List;
 
 public class ProjectList implements Serializable{
 
-    private ListProperty<ObjectProperty<Project>> projets;
+    private ListProperty<ObjectProperty<Project>> projects;
 
     public ProjectList(){
-
+        projects = new SimpleListProperty<>();
     }
 
-    public List<ObjectProperty<Project>> getProjets( ){
-        return projets.get();
+    public List<ObjectProperty<Project>> getProjects( ){
+        return projects.get();
     }
 
-    public ObjectProperty<Project> getProjet( int index){
-        return projets.get( index);
+    public ObjectProperty<Project> getProject( int index){
+        return projects.get( index);
     }
 
+    public ListProperty<ObjectProperty<Project>> projectsProperty() {
+        return projects;
+    }
+
+    public void setProject( int index, Project project){
+        ObjectProperty<Project> projectp = new SimpleObjectProperty<>();
+        projectp.set( project);
+        projects.set( index, projectp);
+    }
+
+    public void removeProject( int index){
+        projects.remove( index);
+    }
+
+    public void addProject( Project project){
+        setProject( projects.size(), project);
+    }
 }
